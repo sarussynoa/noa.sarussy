675b3b263fd10e4b4ccac75f4b5f9eef4e097dbe
noasarussy <noa.sarussy@gmail.com>
2021-05-07 19:56:53 +0300

    assignment 7

--- CV/cv.js
+++ CV/cv.js
@@ -7,3 +7,27 @@ function toggleModal(e) {
     popup.classList.toggle('show')
   }
 }
+
+function getUsers(eId) {
+    var obj, dbParam, xmlhttp, myObj, x, txt = "";
+    obj = { table: "users", limit: 200 };
+    dbParam = JSON.stringify(obj);
+    xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        myObj = JSON.parse(this.responseText);
+        txt += "<table border='1'>"
+        for (x in myObj.data) {
+          txt += "<tr><td>" + myObj.data[x].id + "</td><td>" + myObj.data[x].email + "</td><td>"
+          + myObj.data[x].first_name + "</td><td>" + myObj.data[x].last_name + '</td><td><img src="'
+          + myObj.data[x].avatar + '" width="512" height="512"></td></tr>';
+        }
+        txt += "</table>"
+        document.getElementById(eId).innerHTML = txt;
+      }
+    };
+    xmlhttp.open("GET", "https://reqres.in/api/users?page=2", true);
+    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xmlhttp.send("x=" + dbParam);
+
+}
