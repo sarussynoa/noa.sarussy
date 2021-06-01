6c2fc86782ba8475b50e0c7ea36cd9529a3c2236
sarussynoa <sarussyn@post.bgu.ac.il>
2021-06-01 22:48:45 +0300

    assignment9

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, render_template, url_for, session, request, redirect
 
 app = Flask(__name__)
+app.secret_key = "mySecretKey"
+
+users = [{'firstname': 'noa', 'lastname': 'sarussy', 'email': 'noasarussy@gmail.com'},
+         {'firstname': 'asaf', 'lastname': 'lotz', 'email': 'asaflotz@gmail.com'},
+         {'firstname': 'moti', 'lastname': 'luhim', 'email': 'motiluhim@gmail.com'},
+         {'firstname': 'avi', 'lastname': 'ron', 'email': 'aviron@gmail.com'}]
 
 
 @app.route('/', methods=['GET'])
@@ -18,9 +24,39 @@ def assignment8():
     return render_template('template.html', title='Hobbies')
 
 
-@app.route('/assignment9', methods=['GET'])
+@app.route('/assignment9', methods=['GET', 'POST'])
 def assignment9():
-    return render_template('assignment9.html')
+    current_method = request.method
+    if current_method == 'GET':
+        if 'firstname' in request.args:
+            username = request.args['firstname']
+            lastname = request.args['lastname']
+            email = request.args['email']
+            if not username and not lastname and not email:
+                return render_template('assignment9.html', search=True, users=users)
+            res = []
+            for user in users:
+                if username in ['', user['firstname']] and lastname in ['', user['lastname']] and email in ['', user['email']]:
+                    res.append(user)
+            if res:
+                return render_template('assignment9.html', search=True, found=True, res=res)
+            else:
+                return render_template('assignment9.html', search=True, found=False)
+        else:
+            return render_template('assignment9.html')
+
+    elif current_method == 'POST':
+        if request.form['username']:
+            session['username'] = request.form['username']
+            session['isLogged'] = True
+        return render_template('assignment9.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session['username'] = ''
+    session['isLogged'] = False
+    return redirect(url_for('assignment9'))
 
 
 if __name__ == '__main__':
